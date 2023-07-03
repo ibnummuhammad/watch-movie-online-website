831cf537eb03f1be8315f3eb1348c6a4f17d3c2e
Ibnu Muhammad <ibnu0muhammad@gmail.com>
2023-07-03 21:07:02 +0700

    Remove figcaption

--- app/public/js/home.js
+++ app/public/js/home.js
@@ -14,9 +14,4 @@ for (let i in files) {
   elmtImg.src = "/pictures/" + title + ".jpg";
   elmtImg.width = "200";
   elmtFig.appendChild(elmtImg);
-  var elmtFigcaption = document.createElement("figcaption");
-  elmtFigcaption.id = "figcaptionId";
-  elmtFig.appendChild(elmtFigcaption);
-  var textFigcaption = document.createTextNode(title);
-  elmtFigcaption.appendChild(textFigcaption);
 }
