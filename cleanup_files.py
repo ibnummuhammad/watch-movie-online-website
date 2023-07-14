5441d771c783e71b12f4749f1c7690033a960ff4
Ibnu Muhammad <ibnu0muhammad@gmail.com>
2023-07-14 18:35:40 +0700

    Add cleanup_files.py

--- cleanup_files.py
+++ cleanup_files.py
@@ -1,13 +1,16 @@
 import os
 
 
-path = "/Volumes/iben/movies/the_spongebob_squarepants_movie"
+delete_list = [".DS_Store"]
+
+path = "/Volumes/iben/movies/top_gun"
 files = os.listdir(path)
 print(files)
 
 for file in files:
-    if file in [".DS_Store"]:
-        os.remove(f"{path}/{file}")
+    for delete in delete_list:
+        if delete in file:
+            os.remove(f"{path}/{file}")
 
 files = os.listdir(path)
 print(files)
