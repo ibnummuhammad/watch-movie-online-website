0645dc2c3a6b1e29e7a036f2a492e20199ae55cb
Ibnu Muhammad <ibnu0muhammad@gmail.com>
2023-07-14 20:13:57 +0700

    Fix code

--- cleanup_files.py
+++ cleanup_files.py
@@ -3,14 +3,12 @@ import os
 
 delete_list = [".DS_Store", "Zone.Identifier"]
 
-path = "/Volumes/iben/movies/top_gun"
-files = os.listdir(path)
-print(files)
+path = "/Volumes/iben/movies"
+movies = os.listdir(path)
 
-for file in files:
-    for delete in delete_list:
-        if delete in file:
-            os.remove(f"{path}/{file}")
-
-files = os.listdir(path)
-print(files)
+for movie in movies:
+    files = os.listdir(f"{path}/{movie}")
+    for file in files:
+        for delete in delete_list:
+            if delete in file:
+                os.remove(f"{path}/{movie}/{file}")
