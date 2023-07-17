4fa6a200a6c7e68bcc3493735f8b446c8235c47e
Ibnu Muhammad <ibnu0muhammad@gmail.com>
2023-07-17 19:20:04 +0700

    Fix title

--- app/app.js
+++ app/app.js
@@ -9,6 +9,17 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.static("/Volumes/iben/"));
 
+function titleCase(str) {
+  var splitStr = str.toLowerCase().split(" ");
+  for (var i = 0; i < splitStr.length; i++) {
+    // You do not need to check if i is larger than splitStr length, as your for does that for you
+    // Assign it back to the array
+    splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+  }
+  // Directly return the joined string
+  return splitStr.join(" ");
+}
+
 // Home
 app.get("/", (req, res) => {
   var files = fs.readdirSync("/Volumes/iben/movies/");
@@ -23,6 +34,7 @@ app.get("/:title", (req, res) => {
   var detail = {
     files: files,
     title: req.params.title,
+    titleCap: titleCase(req.params.title.replaceAll("_", " ")),
   };
   res.render("movie_list", detail);
 });
@@ -34,6 +46,7 @@ app.get("/:title/:eps", (req, res) => {
     epsPrev: zeroPad(parseInt(req.params.eps) - 1, 2),
     epsNext: zeroPad(parseInt(req.params.eps) + 1, 2),
     title: req.params.title,
+    titleCap: titleCase(req.params.title.replaceAll("_", " ")),
   };
   res.render("movie_detail", detail);
 });
